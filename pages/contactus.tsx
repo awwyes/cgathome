76c18c900db0c68a79021547c211707e0b47de07
Tony Burks <tonyburks@me.com>
2022-05-04 16:53:14 -0500

    Added Contact Us Page

---
+++ pages/contactus.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer/Footer";
+import TopNavBar from "../components/TopNavBar/TopNavbar";
+import ContactUsPage from "../components/ContactUsPage/ContactUs";
+
+export default function ContactUs() {
+    return (
+        <>
+            <TopNavBar />
+            <ContactUsPage />
+            <Footer />
+        </>
+  );
+};
